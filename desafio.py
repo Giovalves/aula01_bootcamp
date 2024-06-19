08d86c561c5d9ef4b2b5641e1e3d9c5c59c1e39a
Giovanna <giovalves2005@yahoo.com.br>
2024-06-19 07:20:06 -0300

    adicionar o desafio

--- desafio.py
+++ desafio.py
@@ -1,19 +1,16 @@
-# Solicita ao usuario que digite seu nome
-nome_usuario = input("Digite o seu nome: ")
+### Desafio - Refatorar o projeto da aula anterior evitando Bugs!
 
-#Solicite ao usuario que digite o valor do seu salário
-#Converte a entrada para um número de ponto flutuante
-
-salario_usuario = float(input("Digite o seu salário: "))
-
-# Solicite ao usuario que digite o valor do bônus recebido
+CONSTANTE_BONUS = 1000
+# 1) Solicita ao usuário que digite seu nome
+nome_usuario = input ("Digite o seu nome: ")
+# 2) Solicita ao usuário que digite o valor do seu salário
+# Converte a entrada para um número de ponto flutuante
+salario_usuario = float(input ("Digite o seu salario: "))
+# 3) Solicita ao usuário que digite o valor do bônus recebido
 # Converte a entrada para um número de ponto flutuante
 bonus_usuario = float(input("Digite o seu bonus: "))
-
-# Calcule o valor do bônus final 
-valor_do_bonus = 1000 + salario_usuario * bonus_usuario
-
-# Imprime a mensagem personalizada incluindo o nome do usuario e o valor
+# 4) Calcule o valor do bônus final
+valor_do_bonus = CONSTANTE_BONUS + salario_usuario *bonus_usuario
+# 5) Imprime a mensagem personalizada incluindo o nome do usuário, salário e bônus
 print(f"O usuario {nome_usuario} possui o bonus de {valor_do_bonus}")
-
-# Bônus: Quantos bugs e riscos você consegue indentificar nesse programa?
+# Bônus: Quantos bugs e riscos você consegue identificar nesse programa?
